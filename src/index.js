514c4bdecb1a0ed2de9bfd7a436a22d1a43ad5fa
practicewithbyron <practicewithbyron@gmail.com>
2024-02-01 21:59:44 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
-import { BrowserRouter } from "react-router-dom";
 import "animate.css";
 
 import App from "./App.js"
 
-
-ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
-    document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
